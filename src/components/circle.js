19be1acb67ffbee9208a25f36a4837cfbf2d9b4f
abhishekshaw1007 <abhishekshaw1007@gmail.com>
2020-11-29 00:54:49 +0530

    Trial 1

--- src/components/circle.js
+++ src/components/circle.js
@@ -1,6 +1,15 @@
 // import using require
+const Shape = require("./shape.js");
 
 // declare class
-
+class Circle extends Shape {
+  constructor(props) {
+    super(props);
+  }
+  calculateArea() {
+    console.log("Calculated Area of Circle");
+  }
+}
 
 // export class using module.exports
+module.exports = Shape;
--- src/components/shape.js
+++ src/components/shape.js
@@ -1,3 +1,15 @@
 // declare class
+class Shape {
+  constructor(color) {
+    this.color = color;
+  }
+  drawShape() {
+    console.log("Shape Drawn");
+  }
+  calculateArea(shape) {
+    console.log("Area Calculated for: " + shape);
+  }
+}
 
 // export class using module.exports
+module.exports = Shape;
